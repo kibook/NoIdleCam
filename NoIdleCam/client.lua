a4e52e810f4e5dba3ad74acb3d805a861a5eeccf
TayMcKenzieNZ <49776183+TayMcKenzieNZ@users.noreply.github.com>
2021-09-20 22:53:43 +1200

    Added toggle to switch Idle Cam On/Off

--- NoIdleCam/client.lua
+++ NoIdleCam/client.lua
@@ -1,10 +1,34 @@
 
 -- Disable Ped & Vehicle Idle Cam
 
+local idle = 1
+
+RegisterCommand('idlecamoff', function() -- help2 31, 167, 9 
+    TriggerEvent('chat:addMessage', {
+        color = {227,8,0},
+        multiline = true,
+        args = {'[COMMANDS]', 'Idle Cam Is Now Off'}
+    })
+    idle = 1
+end)
+
+RegisterCommand('idlecamon', function() -- help2 31, 167, 9 
+    TriggerEvent('chat:addMessage', {
+        color = {31,167,9},
+        multiline = true,
+        args = {'[COMMANDS]', 'Idle Cam Is Now On'}
+    })
+    idle = 0
+end)
+
 Citizen.CreateThread(function()
     while true do
+    if idle > 0 then
         InvalidateIdleCam()
         InvalidateVehicleIdleCam()
-        Wait(1000) --The idle camera activates after 30 second so we don't need to call this per frame
+        Citizen.Wait(10000)
+    else
+        Citizen.Wait(3300)
     end
+end
 end)
