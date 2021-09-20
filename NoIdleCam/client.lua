9e2480accae84df315e7af9a869f0fe7b3788556
TayMcKenzieNZ <49776183+TayMcKenzieNZ@users.noreply.github.com>
2021-09-20 18:09:16 +1200

    Added toggle to turn on/off Ped & Vehicle Idle Cam
    toggle_idlecam

--- NoIdleCam/client.lua
+++ NoIdleCam/client.lua
@@ -1,10 +1,19 @@
 
 -- Disable Ped & Vehicle Idle Cam
 
+local idleCamEnabled = false
+
+RegisterCommand("toggle_idlecam", function(source, args, raw)
+    idleCamEnabled = not idleCamEnabled
+end, false)
+
 Citizen.CreateThread(function()
     while true do
-        InvalidateIdleCam()
-        InvalidateVehicleIdleCam()
+        if not idleCamEnabled then
+            InvalidateIdleCam()
+            InvalidateVehicleIdleCam()
+        end
         Wait(1000) --The idle camera activates after 30 second so we don't need to call this per frame
     end
+end)
 end)
