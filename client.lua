9bf46f42c8a083019d139391fd9c11fb6e813812
TayMcKenzieNZ <49776183+TayMcKenzieNZ@users.noreply.github.com>
2021-09-21 02:14:11 +1200

    Added Chat Suggestions

--- client.lua
+++ client.lua
@@ -1,31 +1,33 @@
 local idleCamDisabled = true
 
-RegisterCommand('idlecamoff', function() -- help2 31, 167, 9 
-    TriggerEvent('chat:addMessage', {
-        color = {227,8,0},
-        multiline = true,
-        args = {'[COMMANDS]', 'Idle Cam Is Now Off'}
-    })
-    idleCamDisabled = true
+RegisterCommand('idlecamoff', function() -- help2 31, 167, 9
+  TriggerEvent('chat:addMessage', {
+    color = {227,8,0},
+    multiline = true,
+    args = {'[COMMANDS]', 'Idle Cam Is Now Off'}
+  })
+  idleCamDisabled = true
 end)
 
-RegisterCommand('idlecamon', function() -- help2 31, 167, 9 
-    TriggerEvent('chat:addMessage', {
-        color = {31,167,9},
-        multiline = true,
-        args = {'[COMMANDS]', 'Idle Cam Is Now On'}
-    })
-    idleCamDisabled = false
+RegisterCommand('idlecamon', function() -- help2 31, 167, 9
+  TriggerEvent('chat:addMessage', {
+    color = {31,167,9},
+    multiline = true,
+    args = {'[COMMANDS]', 'Idle Cam Is Now On'}
+  })
+  idleCamDisabled = false
 end)
 
 Citizen.CreateThread(function()
-    while true do
-   if idleCamDisabled then
-        InvalidateIdleCam()
-        InvalidateVehicleIdleCam()
-        Citizen.Wait(10000)
+  TriggerEvent("chat:addSuggestion", "/idlecamon", "Re-enables the idle cam")
+  TriggerEvent("chat:addSuggestion", "/idlecamoff", "Disables the idle cam")
+  while true do
+    if idleCamDisabled then
+      InvalidateIdleCam()
+      InvalidateVehicleIdleCam()
+      Citizen.Wait(10000)
     else
-        Citizen.Wait(3300)
+      Citizen.Wait(3300)
     end
-end
+  end
 end)
