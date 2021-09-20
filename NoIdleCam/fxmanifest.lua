84aad72b4bf08d2048c9373de070174f7bfe2647
TayMcKenzieNZ <49776183+TayMcKenzieNZ@users.noreply.github.com>
2021-09-20 15:47:44 +1200

    Fixed Formatting Of fxmanifest

--- NoIdleCam/fxmanifest.lua
+++ NoIdleCam/fxmanifest.lua
@@ -1,6 +1,11 @@
-fx_version 'adamant'
+fx_version 'cerulean'
 game 'gta5'
-version '1.0'
-author 'TayMcKenzieNZ'
 
 client_script 'client.lua'
+
+
+
+author 'TayMcKenzieNZ'
+description 'No Idle Cam'
+version '1.0.0'
+ url 'https://github.com/TayMcKenzieNZ/NoIdleCam'
