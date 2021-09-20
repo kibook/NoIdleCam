8f19824307346288cb4065d76f867300066684f4
TayMcKenzieNZ <49776183+TayMcKenzieNZ@users.noreply.github.com>
2021-09-20 20:02:13 +1200

    Reverted config

--- NoIdleCam/client.lua
+++ NoIdleCam/client.lua
@@ -1,19 +1,10 @@
 
 -- Disable Ped & Vehicle Idle Cam
 
-local idleCamEnabled = false
-
-RegisterCommand("toggle_idlecam", function(source, args, raw)
-    idleCamEnabled = not idleCamEnabled
-end, false)
-
 Citizen.CreateThread(function()
     while true do
-        if not idleCamEnabled then
-            InvalidateIdleCam()
-            InvalidateVehicleIdleCam()
-        end
+        InvalidateIdleCam()
+        InvalidateVehicleIdleCam()
         Wait(1000) --The idle camera activates after 30 second so we don't need to call this per frame
     end
-end)
 end)
