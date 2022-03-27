982d61ad7526a4bbc7720ec26da5cec2037c8603
kibook <kibuzhj@gmail.com>
2022-03-27 16:43:24 -0400

    Use DisableIdleCam native

--- client.lua
+++ client.lua
@@ -1,12 +1,10 @@
-local idleCamDisabled = GetResourceKvpString("idleCam") ~= "on"
-
 RegisterCommand('idlecamoff', function() -- help2 31, 167, 9
   TriggerEvent('chat:addMessage', {
     color = {227,8,0},
     multiline = true,
     args = {'[COMMANDS]', 'Idle Cam Is Now Off'}
   })
-  idleCamDisabled = true
+  DisableIdleCam(true)
   SetResourceKvp("idleCam", "off")
 end)
 
@@ -16,20 +14,14 @@ RegisterCommand('idlecamon', function() -- help2 31, 167, 9
     multiline = true,
     args = {'[COMMANDS]', 'Idle Cam Is Now On'}
   })
-  idleCamDisabled = false
-    SetResourceKvp("idleCam", "on")
+  DisableIdleCam(false)
+  SetResourceKvp("idleCam", "on")
 end)
 
 Citizen.CreateThread(function()
   TriggerEvent("chat:addSuggestion", "/idlecamon", "Re-enables the idle cam")
   TriggerEvent("chat:addSuggestion", "/idlecamoff", "Disables the idle cam")
-  while true do
-    if idleCamDisabled then
-      InvalidateIdleCam()
-      InvalidateVehicleIdleCam()
-      Citizen.Wait(10000)
-    else
-      Citizen.Wait(3300)
-    end
-  end
+  
+  local idleCamDisabled = GetResourceKvpString("idleCam") ~= "on"
+  DisableIdleCam(idleCamDisabled)
 end)
